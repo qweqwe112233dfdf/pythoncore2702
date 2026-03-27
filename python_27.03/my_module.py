473b675d77636bea198cc299d3da6aa769250b3e
qweqwe112233dfdf <radchenkodmytro09@gmail.com>
2026-03-27 19:30:32 +0200

    1232131

---
+++ python_27.03/my_module.py
@@ -0,0 +1,11 @@
+def greeting():
+    print('========== Hello, stranger! ==========')
+
+some_value = 10
+
+turtle_settings = {
+    'shape': 'turtle',
+    'pensize': 5,
+    'pencolor': '#32c18f',
+    'fillcolor': '#eeff09'
+}
